8e75cecfcdd999d25fd2f8241ea4c0a1822b575f
Viktoriya Eliseeva <viktoriya.eliseeva.2002@mail.ru>
2023-05-14 15:48:21 +0300

    first commit in first_branch

--- lab6.cpp
+++ lab6.cpp
@@ -28,6 +28,7 @@ void mergeArrays(int arr1[], int n1, int arr2[], int n2, int arr3[]) {
 }
 
 // функция для вывода объединенного массива на экран
+//first commit in first_branch
 void printArray(int arr[], int n) {
     cout << "Объединенный массив: ";
     for(int i=0; i<n; i++) {
